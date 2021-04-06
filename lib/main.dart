d02be60346088c124d2bdfa7f3b0bcc32e2f7b3f
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-04-06 18:35:27 +0800

    deleted files

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+import 'package:coronavirus_tracker_ph/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'services/APIResponseModel.dart';
 
 void main() {
   runApp(CovidTracker());
 }
 
-class CovidTracker extends StatelessWidget {
+class CovidTracker extends StatefulWidget {
+  CovidTracker({Key key}) : super(key: key);
+
+  @override
+  _CovidTrackerState createState() => _CovidTrackerState();
+}
+
+class _CovidTrackerState extends State<CovidTracker> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-
+    return Container(
+       child: child,
     );
   }
 }
