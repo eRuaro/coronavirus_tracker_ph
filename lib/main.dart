f3c3bd0f6a773fb00bc4f523dc98000c5cbf3651
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-04-10 20:26:43 +0800

    Added active, and total cases

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,42 @@ class CovidTracker extends StatefulWidget {
 }
 
 class _CovidTrackerState extends State<CovidTracker> {
-  String _status = '';
+  String _status;
+  String _totalCases;
   String _activeCases;
 
   void getStatus() async {
     final network = Networking();
     final data = await network.getData();
     final status = data['status'].toString();
-    final phCases = data['data']['regions']['philippines']['total_cases'].toString();
+    final phCases =
+        data['data']['regions']['philippines']['total_cases'].toString();
+    final phActiveCases =
+        data['data']['regions']['philippines']['active_cases'].toString();
 
     setState(() {
       _status = status;
-      _activeCases = phCases;
+      _totalCases = phCases;
+      _activeCases = phActiveCases;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("API Crashcourse"),
-        centerTitle: true,
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(_status),
-            if (_activeCases != null) Text(_activeCases),
+            if (_status != null) Text(_status),
+            if (_totalCases != null) Text(_totalCases),
+            if (_activeCases != null) Text('Cases: $_activeCases'),
+            ElevatedButton(
+              onPressed: () => getStatus(),
+              child: Text('Get Data'),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => getStatus(),
       ),
     );
   }
