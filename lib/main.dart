abbee2243afb4c26c297e05d6ed4932affb4a80d
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-04-10 20:30:26 +0800

    Added deaths, and recovered data

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class _CovidTrackerState extends State<CovidTracker> {
   String _status;
   String _totalCases;
   String _activeCases;
+  String _deaths;
+  String _recovered;
 
   void getStatus() async {
     final network = Networking();
@@ -27,11 +29,17 @@ class _CovidTrackerState extends State<CovidTracker> {
         data['data']['regions']['philippines']['total_cases'].toString();
     final phActiveCases =
         data['data']['regions']['philippines']['active_cases'].toString();
+    final phDeathCases =
+        data['data']['regions']['philippines']['deaths'].toString();
+    final phRecoveredCases =
+        data['data']['regions']['philippines']['recovered'].toString();
 
     setState(() {
       _status = status;
       _totalCases = phCases;
       _activeCases = phActiveCases;
+      _deaths = phDeathCases;
+      _recovered = phRecoveredCases;
     });
   }
 
@@ -43,8 +51,10 @@ class _CovidTrackerState extends State<CovidTracker> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             if (_status != null) Text(_status),
-            if (_totalCases != null) Text(_totalCases),
-            if (_activeCases != null) Text('Cases: $_activeCases'),
+            if (_totalCases != null) Text('Total Cases: $_totalCases'),
+            if (_activeCases != null) Text('Active Cases: $_activeCases'),
+            if (_deaths != null) Text('Deaths: $_deaths'),
+            if (_recovered != null) Text('Recovered: $_recovered'),
             ElevatedButton(
               onPressed: () => getStatus(),
               child: Text('Get Data'),
