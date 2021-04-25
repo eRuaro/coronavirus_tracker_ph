2cc623c47dc38f7caa9e321cca1c3c5c1750c53d
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-04-25 13:00:14 +0800

    took out cubit

--- lib/cubit/covid_data_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:coronavirus_tracker_ph/networking/networking.dart';
-
-part 'covid_data_state.dart';
-
-class CovidDataCubit extends Cubit<CovidDataState> {
-  CovidDataCubit() : super(CovidDataInitial());
-}
